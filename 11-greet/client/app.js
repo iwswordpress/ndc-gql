584453e8eead1324a58b46370c2b5f1ac72463f6
iwswordpress <iwswordpress@gmail.com>
2021-06-29 22:52:29 +0100

    greet

---
+++ 11-greet/client/app.js
@@ -0,0 +1,28 @@
+const URL = 'http://localhost:5000';
+
+const output = 'TX-' + Math.floor(Math.random() * 100);
+async function fetchMessage() {
+	const response = await fetch(URL, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify({
+			query: `
+       query{
+				greet(firstName:"Ted" )
+			}
+
+     `,
+		}),
+	});
+
+	const { data } = await response.json();
+
+	return data;
+}
+
+fetchMessage().then((data) => {
+	console.log(data.greet);
+	document.querySelector('output').textContent = data.greet;
+});
---
+++ 11-greet/index.js
@@ -0,0 +1,28 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+	type Query {
+		greet(firstName: String!): String!
+	}
+	schema {
+		query: Query
+	}
+`;
+
+const resolvers = {
+	Query: {
+		greet: (parent, args, context, info) => {
+			console.log(args.firstName);
+			return `Hello ${args.firstName}`;
+		},	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 5000 }).then(({ url }) => console.log(`Server running at port ${url}`));
+
+// EXPERIMENTS:
+// Can we use any variable instead of typeDefs or resolvers?
+// Do we need the schema field with this set up?
+// Can we change Query to Q? Can we do this if we remove the schema field?
+// Change the getMessage to something else? Does it work? How do we fix this?
