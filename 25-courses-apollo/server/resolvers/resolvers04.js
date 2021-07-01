adaadd8853335d0c757a1904629b6982fc5d5c08
iwswordpress <iwswordpress@gmail.com>
2021-07-01 09:53:38 +0100

    JSON_URL

--- 25-courses-apollo/server/resolvers/resolvers04.js
+++ 25-courses-apollo/server/resolvers/resolvers04.js
@@ -1,39 +1,40 @@
 const fetch = require('node-fetch');
 
+const JSON_URL = 'http://localhost:4010';
 const Query = {
 	courses: async () => {
-		const allCourses = await fetch('http://localhost:4010/courses');
+		const allCourses = await fetch(`${JSON_URL}/courses`);
 		const result = await allCourses.json();
-		console.log(`AJAX: http://localhost:4010/courses/`);
+		console.log(`AJAX: ${JSON_URL}/courses/`);
 		console.log(result);
 
 		return result;
 	},
 	course: async (root, { id }) => {
 		// id = 101;
-		const course = await fetch(`http://localhost:4010/courses/${id}`);
+		const course = await fetch(`${JSON_URL}/courses/${id}`);
 		const result = await course.json();
 		console.log(`AJAX: http://localhost:4010/courses/${id}`);
 		console.log(result);
 		return result;
 	},
 	tutors: async () => {
-		const allTutors = await fetch('http://localhost:4010/tutors');
+		const allTutors = await fetch(`${JSON_URL}/tutors`);
 		const result = await allTutors.json();
-		console.log(`AJAX: http://localhost:4010/tutors/`);
+		console.log(`AJAX:http://localhost:4010/tutors/`);
 		console.log(result);
 
 		return result;
 	},
 	tutor: async (root, { id }) => {
-		const tutor = await fetch(`http://localhost:4010/tutors/${id}`);
+		const tutor = await fetch(`${JSON_URL}/tutors/${id}`);
 		const result = await tutor.json();
 		console.log(`AJAX: http://localhost:4010/tutors/${id}`);
 		console.log(result);
 		return result;
 	},
 	students: async () => {
-		const allStudents = await fetch('http://localhost:4010/students');
+		const allStudents = await fetch(`${JSON_URL}/students`);
 		const result = await allStudents.json();
 		console.log(`AJAX: http://localhost:4010/students/`);
 		console.log(result);
@@ -41,7 +42,7 @@ const Query = {
 		return result;
 	},
 	student: async (root, { id }) => {
-		const student = await fetch(`http://localhost:4010/students/${id}`);
+		const student = await fetch(`${JSON_URL}/students/${id}`);
 		const result = await student.json();
 		console.log(`AJAX: http://localhost:4010/student/${id}`);
 		console.log(result);
@@ -53,7 +54,7 @@ const Query = {
 const Course = {
 	// course is the parent object
 	tutor: (course, args, ctx, info) => {
-		console.log('Course parent having tutor:Tutor resolved', course);
+		console.log(`Course parent having tutor:Tutor resolved`, course);
 		return { id: '11111', firstName: 'A', lastName: 'Tutor', email: 'x@test.com' };
 	},
 };
