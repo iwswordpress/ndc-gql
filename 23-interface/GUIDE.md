2e8267ebc4780dd4825c9c60bb0fb176ee423a3d
iwswordpress <iwswordpress@gmail.com>
2021-07-04 09:51:36 +0100

    23 Interface

---
+++ 23-interface/GUIDE.md
@@ -0,0 +1,29 @@
+## 23-interface
+
+Interfaces allow us to group result into one query.
+
+We have to copy all interface fields in the extended type.
+
+It is not a labour saving method for the SERVER but for the CLIENT.
+
+Furthermore, the definition of the interface might be in another file and would no necessarily know what the interface included.
+
+GQL wants the schema to be fully self-documenting so we have to copy its fields. In reality, we do this once, but the CLIENT may do this many, many times.
+
+![gql](/_images/23-interface-query.png)
+
+Like UNION, GQL will need to know what type of the interface it is working with.
+
+We carry out the Query for data in the same way but must add:
+
+```
+	Animal: {
+		__resolveType(animal, context, info) {
+			console.log(`__resolving Animal type as ---> ${animal.species}`);
+			return animal.species;
+		},
+	},
+
+```
+
+It is saying 'The type of animal you are working with is ...'
---
+++ 23-interface/index.js
@@ -0,0 +1,62 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+	interface Animal {
+		species: String
+	}
+	#  caps for learning purpose not how it is done
+	type Tiger implements Animal {
+		species: String
+		stripeCount: Int
+	}
+	type Lion implements Animal {
+		species: String
+		color: String
+	}
+
+	type Query {
+		animals: [Animal]
+	}
+
+	# schema is included by default but shows why query is a reserved work in playground
+	schema {
+		query: Query
+	}
+`;
+const resolvers = {
+	// This is where the usual return of data goes...
+	Query: {
+		animals: () => {
+			console.log('Query > animals and giving typs of species');
+
+			return [
+				{
+					species: 'Tiger',
+					stripeCount: 22,
+				},
+				{ species: 'Lion', color: 'RED' },
+			];
+		},
+	},
+	// We need this extra resolver function to let GQL know which type of Animal it is working with
+	/*
+THIS WILL BE ERROR WITHOUT 
+ "message": "Abstract type Animal must resolve to an Object type at runtime for field Query.animals with value { species: \"Tiger\", stripeCount: 22 }, received \"undefined\". Either the Animal type should provide a \"resolveType\" function or each possible type should provide an \"isTypeOf\" function.",
+
+	*/
+	Animal: {
+		__resolveType(animal, context, info) {
+			console.log(`__resolving Animal type as ---> ${animal.species}`);
+			return animal.species;
+		},
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+server.listen({ port: 5000 }).then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+});
