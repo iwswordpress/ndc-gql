8418805124ff2add3563c56dc06a2b940f59ee37
iwswordpress <iwswordpress@gmail.com>
2021-07-08 15:22:59 +0100

    COURSES-01

---
+++ COURSES-01/client/app04.js
@@ -0,0 +1,35 @@
+const URL = 'http://localhost:5000/graphql';
+
+fetchMessage(4).then((data) => {
+	console.log(data.getProjectById);
+	document.querySelector('output').textContent = data.getProjectById.name;
+});
+// if id used, need $id, it is not just an empty parameter with any reference.
+// replacing $id as $y does not work in client
+async function fetchMessage(x) {
+	// use something other for clarity
+	console.log('x =', x);
+	const response = await fetch(URL, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify({
+			query: `
+				query ProjectQuery($id: ID!) {
+					getProjectById(id: $id) {
+						id
+						name
+					}
+				}
+
+     `,
+			variables: { id: x }, // x used to keep clarity on ids. Must use id: $id
+			// { "id": 3 } is query varaible inplayground. No $
+		}),
+	});
+
+	const { data } = await response.json();
+
+	return data;
+}
