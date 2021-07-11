99aee5145b7e1c484fe559c25866d8a5f371d777
iwswordpress <iwswordpress@gmail.com>
2021-07-11 09:18:15 +0100

    loaders

--- COURSES-03/40-caching/dataloader.js
+++ COURSES-03/40-caching/dataloader.js
@@ -13,24 +13,33 @@ const batchUsers = async (ids) => {
 };
 
 const batchUserLoader = new DataLoader((keys) => batchUsers(keys));
-
+console.log('START INIT', Date.now());
 batchUserLoader.load(1);
 batchUserLoader.load(2);
 batchUserLoader.load(1);
 batchUserLoader.load(1);
+
 batchUserLoader.load(1);
 batchUserLoader.load(3);
+batchUserLoader.load(9);
+batchUserLoader.load(10);
+console.log('END INIT', Date.now());
 
 // Force next-tick
 setTimeout(() => {
+	console.log('==================================');
+	console.log('START TIMEOUT 1', Date.now());
 	batchUserLoader.load(3);
 	batchUserLoader.load(4);
 	batchUserLoader.load(5);
 	batchUserLoader.load(6);
+	console.log('END TIMEOUT 1', Date.now());
 }, 100);
 
 // Force next-tick
 setTimeout(() => {
+	console.log('==================================');
+	console.log('START TIMEOUT 2', Date.now());
 	batchUserLoader.load(1);
 	batchUserLoader.load(2);
 	batchUserLoader.load(3);
@@ -43,4 +52,8 @@ setTimeout(() => {
 	batchUserLoader.load(10);
 	batchUserLoader.load(11);
 	batchUserLoader.load(12);
+	batchUserLoader.load(13);
+	batchUserLoader.load(14);
+	batchUserLoader.load(15);
+	console.log('END TIMEOUT 2', Date.now());
 }, 200);
