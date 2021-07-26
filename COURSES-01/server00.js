3dfceb0f87543df3c0b1aeea3312964b6ba9bbdc
iwswordpress <iwswordpress@gmail.com>
2021-07-26 09:53:58 +0100

    COURSE-01

--- COURSES-01/server00.js
+++ COURSES-01/server00.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// use .env for PORT at this stage
 const dotEnv = require('dotenv');
 
 dotEnv.config();
@@ -9,9 +10,14 @@ const typeDefs = gql`
 		"""
 		This is field level comment in docs - better example in server02 as there are more root types.
 		"""
-		hello: String! # comment
+		hello: String # comment
 	}
 	# schema is included by default but shows why query is a reserved work in playground
+	# rename Query to Q...?
+	# comment out schema {}...?
+	# remove resolver hello...?
+	# use hello: String! and change resolver accordingly.
+	# look at DFev > Network tab... more detail in Server01.js
 	schema {
 		query: Query ## Query could be changed to anything else but not advised.
 	}
