7a9f6a9c52f3d38899b7114171501e63a0068c53
iwswordpress <iwswordpress@gmail.com>
2021-07-28 22:51:49 +0100

    restructure

--- XXX-ORIGINAL-COURSES-02/mergeSchemas/mergeTypes.js
+++
@@ -1,7 +0,0 @@
-const { mergeTypeDefs } = require('@graphql-tools/merge');
-const clientType = require('./clientType');
-const productType = require('./productType');
-
-const types = [clientType, productType];
-
-module.exports = mergeTypeDefs(types);
