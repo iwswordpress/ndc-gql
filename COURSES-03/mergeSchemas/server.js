f9fecdfb06b4a83f5cfd16c34d39f4acb085f007
iwswordpress <iwswordpress@gmail.com>
2021-08-17 09:41:25 +0100

    correct courses-03

--- COURSES-03/mergeSchemas/server.js
+++ COURSES-03/mergeSchemas/server.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: PORT }).then(({ url }) => {
 	console.log('====================================');
-	console.log(`Server04 running at port ${url}`);
+	console.log(`Server running at port ${url}`);
 	console.log('!!!');
 	console.log(`PLEASE NOTE: resolvers file is empty...just showing that the schema is merged...`);
 });
