887772b3495e08513d7da596da2bf8039d75bee5
iwswordpress <iwswordpress@gmail.com>
2021-08-19 09:36:55 +0100

    add back students and projects

---
+++ COURSES-03/data/students.js
@@ -0,0 +1,20 @@
+module.exports.students = [
+	{
+		id: 1,
+		name: 'SALLY',
+		email: 'sally@test.com',
+		projects: [1, 2, 5, 7],
+	},
+	{
+		id: 2,
+		name: 'ted',
+		email: 'ted@test.com',
+		projects: [3, 6, 2],
+	},
+	{
+		id: 3,
+		name: 'anne',
+		email: 'anne@test.com',
+		projects: [4],
+	},
+];
